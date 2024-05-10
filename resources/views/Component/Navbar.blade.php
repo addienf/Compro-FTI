2d9e536d04060f3e7868471765587b704c19fadd
addienaf <addienaf2001@gmail.com>
2024-05-11 05:14:55 +0700

    Template Awal

---
+++ resources/views/Component/Navbar.blade.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="{{ asset('css\navbar.css') }}">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<div class="stroke navbar" id="mainNav">
+    <img src="img\itenass.png" width="100px" height="25px"alt="Logo image" />
+    <ul>
+        <li><a href="#">Profil</a></li>
+        <li><a href="#">Akademik</a></li>
+        <li><a href="#">Kemahasiswaan</a></li>
+        <li><a href="#">MBKM</a></li>
+        <li><a href="#">Tracer Study</a></li>
+        <li><a href="#">Kerjasama</a></li>
+        <li><a href="#">Dokumen</a></li>
+    </ul>
+</div>
+<div class="navbottom stroke">
+    <ul>
+        <li><a href="#home">Penelitian</a></li>
+        <li><a href="#about">PKM</a></li>
+        <li><a href="#contact">Publikasi</a></li>
+        <li><a href="#contact">Penjaminan Mutu</a></li>
+    </ul>
+</div>
