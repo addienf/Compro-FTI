60d522303ca3c1beea5f4b0d847083338b2ae2fe
162020022 Muhammad Gaizka Agusti <muhammad.gaizka@mhs.itenas.ac.id>
2024-05-26 23:24:15 +0700

    Membuat halaman login

--- resources/views/Content/Login.blade.php
+++ resources/views/Content/Login.blade.php
@@ -1,18 +1,25 @@
 @extends('Layout.layout')
 @section('title', 'Login Admin')
-{{-- <link rel="stylesheet" href="{{ asset('css\style3row.css') }}"> --}}
+<link rel="stylesheet" href="{{ asset('css\login.css') }}">
 @section('content')
-    <h1>Login</h1>
-    <form method="POST" action="post-login">
-        @csrf
-        <div>
-            <label>Email:</label>
-            <input type="email" name="email" required>
+
+    <div class="login-page">
+        <div class="form">
+            <div class="login-form">
+                <form method="POST" action="post-login">
+
+                    @csrf
+                    <label>Form Login</label>
+                    <div>
+                        <input type="email" name="email" required placeholder="Email">
+                    </div>
+                    <div>
+                        <input type="password" name="password" required placeholder="Password">
+                    </div>
+                    <button type="submit">Login</button>
+                    <p class="message">Fakultas Teknologi Industri</a></p>
+                </form>
+            </div>
         </div>
-        <div>
-            <label>Password:</label>
-            <input type="password" name="password" required>
-        </div>
-        <button type="submit">Login</button>
-    </form>
+    </div>
 @endsection
