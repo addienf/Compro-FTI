ca751da003a74e8f517bd637f5873e00a5a70436
162020022 Muhammad Gaizka Agusti <muhammad.gaizka@mhs.itenas.ac.id>
2024-05-26 23:02:30 +0700

    Membenarkan halaman, dok,publikasi, penjaminan mutu

--- resources/views/Content/Dokumen.blade.php
+++ resources/views/Content/Dokumen.blade.php
@@ -1,146 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dokumen</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-
-
-    <style>
-        .container {
-            padding: 20px;
-        }
-
-        .bag1 {
-            background-color: #E6DFF0;
-            font-weight: semibold;
-            height: 120px;
-            padding: 45px;
-
-        }
-
-
-        .subab {
-            font-size: 32px;
-            font-weight: semibold;
-        }
-
-
-        .subab2 {
-            font-family: "Source Sans", Arial, sans-serif;
-            font-size: 32px;
-            font-weight: semibold;
-            text-align: center;
-
-        }
-
-
-        .paragraf {
-            font-size: 24px;
-            font-family: Inter;
-        }
-
-        .gambar {
-            position: center;
-
-        }
-
-        .btn-purple {
-            background-color: #261446;
-            /* Warna ungu */
-            color: #fff;
-            /* Warna teks putih */
-        }
-
-        .bagteks {
-            font-family: "Source Sans", Arial, sans-serif;
-            font-size: 40px;
-            font-weight: semibold;
-        }
-
-        .bagjudulteks {
-            font-weight: bold;
-
-        }
-
-
-        .judulrenstra {
-            width: 530px;
-            height: 50px;
-            background: white;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-            color: black;
-            text-align: center;
-            margin
-        }
-
-        .judulrenstra2 {
-            width: 530px;
-            height: 50px;
-            color: black;
-            text-align: center;
-        }
-
-
-        .btn,
-        btn-purple {
-            width: 400px;
-        }
-
-        .btn-purple {
-            background-color: #261446;
-            color: #fff;
-            width: 500px;
-            height: 50px;
-            border-radius: 10px;
-        }
-
-        .btn-purple:hover {
-            background-color: #3e1f6d;
-            /* Warna ungu lebih terang saat hover */
-            color: #fff;
-            /* Warna teks tetap putih */
-        }
-
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        td,
-        th {
-            text-align: center;
-            font-family: "Source Sans", Arial, sans-serif;
-            padding: 10px;
-            font-size: 40px;
-            font-weight: bold;
-
-        }
-
-        tr:nth-child(even) {
-            background-color: ;
-        }
-
-        .h4 {
-            font-family: "Source Sans", Arial, sans-serif;
-            font-weight: bold;
-        }
-
-        .judulatas {
-            background: white;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.4);
-            font-weight: bold;
-        }
-    </style>
-
-
-</head>
-
-<body>
-
+@extends('Layout.layout')
+<link rel="stylesheet" href="{{ asset('css\dokumen.css') }}">
+@include('Component.Navbar')
+@section('content')
     {{--  bagian ke-1  --}}
     <div class="container1">
         <div class="bag1">
@@ -182,87 +43,68 @@
         </div>
     </div>
 
-
-
     <div class="container">
         <div class="subab">Renstra</div>
+
+        <div class="kotak">
+            <div class="judulatas1">
+                Judul
+            </div>
+            <div class="judulatas2">
+                Link
+            </div>
+        </div>
+
+
+        <table>
+            <tr>
+                <td>
+                    <h5> Jurnal Penelitian </h5>
+                </td>
+                <td> <button type="button" class="btn btn-purple">www.Jurnal.com</button> </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5> Jurnal PKM </h5>
+                </td>
+                <td> <button type="button" class="btn btn-purple">www.PKM.org</button> </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5> Diseminasi FTI </h5>
+                </td>
+                <td> <button type="button" class="btn btn-purple">www.DiseminasiFTI.org</button> </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5> Seminar Nasional </h5>
+                </td>
+                <td> <button type="button" class="btn btn-purple">www.Semnas.org</button> </td>
+            </tr>
+            <tr>
+                <td>
+                    <h5> FoITIC </h5>
+                </td>
+                <td> <button type="button" class="btn btn-purple">www.foitic.org</button> </td>
+            </tr>
+        </table>
+
         <div class="container">
-
-
-
-            <div class="container">
-                <table>
-
-                    <tr class="judulatas">
-                        <th>
-                            <h3> Nama </h3>
-                        </th>
-                        <th>
-                            <h3> Link </h3>
-                        </th>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            <h4> Jurnal Penelitian </h4>
-                        </td>
-                        <td> <button type="button" class="btn btn-purple">www.Jurnal.com</button> </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <h4> Jurnal PKM </h4>
-                        </td>
-                        <td> <button type="button" class="btn btn-purple">www.PKM.org</button> </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <h4> Diseminasi FTI </h4>
-                        </td>
-                        <td> <button type="button" class="btn btn-purple">www.DiseminasiFTI.org</button> </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <h4> Seminar Nasional </h4>
-                        </td>
-                        <td> <button type="button" class="btn btn-purple">www.Semnas.org</button> </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <h4> FoITIC </h4>
-                        </td>
-                        <td> <button type="button" class="btn btn-purple">www.foitic.org</button> </td>
-                    </tr>
-
-                </table>
+            <div class="bag3">
+                <div class="subab2">Laporan Kinerja</div>
+                <img src="https://asset.kompas.com/data/photo/2021/04/23/60828b8b00b4e.jpg" alt="Deskripsi gambar"
+                    class="d-block mx-auto">
             </div>
+        </div>
 
 
-
-
-
-            <div class="container">
-                <div class="bag3">
-                    <div class="subab2">Laporan Kinerja</div>
-                    <img src="https://asset.kompas.com/data/photo/2021/04/23/60828b8b00b4e.jpg" alt="Deskripsi gambar"
-                        class="d-block mx-auto">
-                </div>
+        <div class="container">
+            <div class="bag3">
+                <div class="subab2">Laporan Tahunan</div>
+                <img src="https://asset.kompas.com/data/photo/2021/04/23/60828b8b00b4e.jpg" alt="Deskripsi gambar"
+                    class="d-block mx-auto">
             </div>
-
-
-            <div class="container">
-                <div class="bag3">
-                    <div class="subab2">Laporan Tahunan</div>
-                    <img src="https://asset.kompas.com/data/photo/2021/04/23/60828b8b00b4e.jpg" alt="Deskripsi gambar"
-                        class="d-block mx-auto">
-
-                </div>
-            </div>
-
-
-            <!-- Bootstrap JS -->
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-
-</html>
+        </div>
+    </div>
+    @include('Component.Footer')
+@endsection
